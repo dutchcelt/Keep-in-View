0e7c8d064e0fa8ee0aac12ee7aa9d627e15e8bfc
Egor Kloos <egor+git@lunatech.com>
2012-02-13 17:15:18 +0100

    Added GPL 3 and SIL licenses

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 Keep in View
 ===========
 
-Copyright &copy; 2011, Lunatech Research B.V., C. Egor Kloos. All rights reserved.
+Copyright &copy; 2011 - 2012, Lunatech Research B.V., C. Egor Kloos. All rights reserved.
+GNU General Public License, version 3 (GPL-3.0)
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  
+If not, see http://www.opensource.org/licenses/gpl-3.0.html
+
 
 ### Description
 Don't allow elements to scroll out of view by having them stick to the top or bottom of a window.
--- keepinview.js
+++ keepinview.js
@@ -1,8 +1,23 @@
 /*! ###########################################################################
-    Author:     Lunatech Research
-                Egor Kloos
-    Date:       November 2011
-    git:        https://github.com/dutchcelt/Keep-in-View
+    
+    Source: https://github.com/dutchcelt/Keep-in-View
+    
+    Copyright (C) 2011 - 2012,  Lunatech Research B.V., C. Egor Kloos. All rights reserved.
+    GNU General Public License, version 3 (GPL-3.0)
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.opensource.org/licenses/gpl-3.0.html
+    
     ########################################################################### */
     
     // Uses AMD or browser globals to create a jQuery plugin.
